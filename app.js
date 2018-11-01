7a0dff19b4d7e4b380468ebae7fbaafa14cec377
Ankur Sharma <ankur.sharma@technocube.in>
2018-11-01 12:55:06 +0530

    added xhr request

---
+++ app.js
@@ -0,0 +1,9 @@
+ var x = new XMLHttpRequest();
+  x.open('GET, 'https://s3.amazonaws.com');
+  x.onload = x.onerror = function () {
+    console.log(x.responseText)
+    var xml = new XMLParser().parseFromString(x.responseText);    
+    console.log(xml)
+    printResult(parseListObjects(xml));
+  }
+   x.send(options.data);
