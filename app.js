a61d5c40127c2b6084b172065941fe0be8fa8544
Ankur Sharma <asb14690@users.noreply.github.com>
2018-11-01 13:00:42 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
  var x = new XMLHttpRequest();
-  x.open('GET, 'https://s3.amazonaws.com');
+  x.open('GET', 'https://s3.amazonaws.com');
   x.onload = x.onerror = function () {
     console.log(x.responseText)
     var xml = new XMLParser().parseFromString(x.responseText);    
